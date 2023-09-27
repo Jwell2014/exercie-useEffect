0262bd3e98b5da88141ab1966ec190eebf50affb
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2023-09-27 11:16:59 +0200

    exercice 4

--- src/composents/Form.js
+++ src/composents/Form.js
@@ -1,66 +1,69 @@
 import React, { useEffect, useState } from 'react';
 
-function Form(){
+function Form() {
+  // Déclaration des états avec useState
+  const [username, setUsername] = useState("");
+  const [login, setLogin] = useState("");
+  const [error, setError] = useState("");
 
-    const [username, setUsername] = useState("");
-    const [login, setLogin] = useState("");
-    const [error, setError] = useState("");
- 
-    useEffect(() => {
-        // Fonction de validation des champs
-        const validateFields = () => {
-          if (username.trim() === '' || login.trim() === '') {
-            setError('Les champs ne doivent pas être vides.');
-          } else {
-            setError('');
-          }
-        };
-    
-        validateFields(); // Appeler la fonction de validation à chaque changement
-    
-      }, [username, login]); // Le tableau de dépendances inclut 'username' et 'password'
-    
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        // Envoyer les données du formulaire (non implémenté dans cet exemple)
-        console.log(username,login)
-        alert(`Nom : ${username} \n Login : ${login}`)
-        setLogin("")
-        setUsername("")
-      };
+  // Utilisation de useEffect pour effectuer des validations en temps réel
+  useEffect(() => {
+    // Fonction de validation des champs
+    if (username.trim() === '' || login.trim() === '') {
+      setError('Les champs ne doivent pas être vides.');
+    } else if (username.length < 4) {
+      setError('Le champ nom ne doit pas être inférieur à 4 caractères.');
+    } else if (login.length < 8) {
+      setError('Le champ login ne doit pas être inférieur à 8 caractères.');
+    } else if (!/[A-Z]/.test(login)) {
+      setError('Le champ login doit contenir au moins une majuscule.');
+    } else if (!/\d/.test(login)) {
+      setError('Le champ login doit contenir au moins un chiffre.');
+    } else {
+      setError('');
+    }
+  }, [username, login]); // Le tableau de dépendances inclut 'username' et 'login'
 
-    return (
-        <div className="fond">
-            <form onSubmit={handleSubmit} >
-                <div>
-                    <label>Nom</label>
-                    <input 
-                        type='text'
-                        value={username}
-                        onChange={(e)=>setUsername(e.target.value)}
-                    ></input>
-                </div>
-               
-                <div>
-                    <label>Login</label>
-                    <input
-                        type='text'
-                        value={login}
-                        onChange={(e)=>setLogin(e.target.value)}
-                    ></input>
-                </div>
-                {error ? (
-                <>    
-                    <p style={{ color: 'red' }}>{error}</p>
-                    <p style={{textDecorationLine:"line-through"}} >Se Connecter</p>
-                </>
-                ) : (
-                <button type="submit">Se Connecter</button>
-                )}
-                
-            </form>
+  // Fonction pour gérer la soumission du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Envoyer les données du formulaire (non implémenté dans cet exemple)
+    console.log(username, login);
+    alert(`Nom : ${username} \n Login : ${login}`);
+    setLogin("");
+    setUsername("");
+  };
+
+  return (
+    <div className="fond">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Nom</label>
+          <input 
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          ></input>
         </div>
-    );
-};
+        <div>
+          <label>Login</label>
+          <input
+            type='text'
+            value={login}
+            onChange={(e) => setLogin(e.target.value)}
+          ></input>
+        </div>
+        {error ? (
+          <>    
+            <p style={{ color: 'red' }}>{error}</p>
+            <p style={{ textDecorationLine: "line-through" }}>Se Connecter</p>
+          </>
+        ) : (
+          <button type="submit">Se Connecter</button>
+        )}
+      </form>
+    </div>
+  );
+}
 
 export default Form;
